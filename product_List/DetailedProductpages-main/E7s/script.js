a5c4f5c8ebe978a3d2c9987e8b03ed642d31198f
Ali <45833494+engrali93@users.noreply.github.com>
2021-09-20 22:08:03 +0200

    timer

--- product_List/DetailedProductpages-main/E7s/script.js
+++ product_List/DetailedProductpages-main/E7s/script.js
@@ -35,8 +35,8 @@ var swiper = new Swiper(".mySwiper", {
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-
-    let sale = "Sep 30, 2021 00:00:00",
+        let sale = $.csv.toObjects("SaleInput.csv"),
+    //let sale = "Sep 30, 2021 00:00:00",
         countDown = new Date(sale).getTime(),
         x = setInterval(function () {
 
